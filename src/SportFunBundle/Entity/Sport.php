71213861a7a5ca5a40b4d76c928610604d0f54d1
Jimmy Hou <ximing.developer@gmail.com>
2015-11-22 18:14:59 +1100

    Build Sport and Stadium entity, CRUD, Controller, install bower, npm, node and bootstrap, jquery

---
+++ src/SportFunBundle/Entity/Sport.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace SportFunBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Sport
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="SportFunBundle\Entity\SportRepository")
+ */
+class Sport
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=100)
+     */
+    private $name;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="type", type="smallint")
+     */
+    private $type;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="status", type="smallint")
+     */
+    private $status;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Sport
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set type
+     *
+     * @param integer $type
+     *
+     * @return Sport
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return integer
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set status
+     *
+     * @param integer $status
+     *
+     * @return Sport
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * Get status
+     *
+     * @return integer
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+}
+
---
+++ src/SportFunBundle/Form/StadiumType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace SportFunBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class StadiumType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('code')
+            ->add('address')
+            ->add('suburb')
+            ->add('state')
+            ->add('postcode')
+            ->add('latitude')
+            ->add('longitude')
+            ->add('status')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'SportFunBundle\Entity\Stadium'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'sportfunbundle_stadium';
+    }
+}
