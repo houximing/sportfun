eaa58b390dd9c9de26c03d84481a8c2414eb49ad
Jimmy Hou <ximing.developer@gmail.com>
2015-11-23 21:37:25 +1100

    create suburb model and crud

---
+++ src/SportFunBundle/Entity/Suburb.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace SportFunBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Suburb
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="SportFunBundle\Entity\SuburbRepository")
+ */
+class Suburb
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="postcode", type="string", length=100)
+     */
+    private $postcode;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="status", type="smallint")
+     */
+    private $status;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Suburb
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set postcode
+     *
+     * @param string $postcode
+     *
+     * @return Suburb
+     */
+    public function setPostcode($postcode)
+    {
+        $this->postcode = $postcode;
+
+        return $this;
+    }
+
+    /**
+     * Get postcode
+     *
+     * @return string
+     */
+    public function getPostcode()
+    {
+        return $this->postcode;
+    }
+
+    /**
+     * Set status
+     *
+     * @param integer $status
+     *
+     * @return Suburb
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * Get status
+     *
+     * @return integer
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+}
+
