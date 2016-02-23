33bef6b3d2ee1d6d34c02ec0715519306a126ce6
Jimmy Hou <ximing.developer@gmail.com>
2016-02-23 17:55:43 +1100

    SF-9 adding plugins for schdule and calendar

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,8 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new SportFunBundle\SportFunBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
+            new ADesigns\CalendarBundle\ADesignsCalendarBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
